bcd945ec92d70393d895f62d3d8ea41d1de14e7a
LusqueFusuqe <145375259+Bannybum@users.noreply.github.com>
2024-11-21 23:44:50 -0300

    é
    isso

--- joguinhoooo/Assets/Scripts/Villain.cs
+++ joguinhoooo/Assets/Scripts/Villain.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Villain : MonoBehaviour
 {
 
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 }
