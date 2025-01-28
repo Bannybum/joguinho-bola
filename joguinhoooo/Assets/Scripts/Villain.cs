039085c5276a383b56813033ab9c06bda4309d20
LusqueFusuqe <145375259+Bannybum@users.noreply.github.com>
2025-01-27 22:41:39 -0300

    Almost
    explosivo

--- joguinhoooo/Assets/Scripts/Villain.cs
+++ joguinhoooo/Assets/Scripts/Villain.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class Villain : MonoBehaviour
 {
+    public Animator animator;
+    private Rigidbody2D rb;
+    private bool isFalling = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (!isFalling && collision.gameObject.CompareTag("Player"))
+        {
+            TriggerFall();
+        }
+    }
+
+    private void TriggerFall()
+    {
+        isFalling = true;
+        rb.gravityScale = 2;
+        animator.SetTrigger("IsFalling");
 
     }
+
 }
